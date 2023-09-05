02427ffddec9b002d05e105691cca2de826b38fc
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-09-06 00:15:27 +0900

    reversed String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) { // apple
+   return str.split('').reduce((rev, char)=> char + rev, '')
+}
+
+reverse('bitna');
 
 module.exports = reverse;
+
+// return str.split('').reverse().join('');
+
+// function reverse(str) {
+//     let reversed = '';
+//     for ( let char of str) { // apple
+//         reversed = char + reversed;
+//     }
+//     return reversed;
+// }
+
+// function reverse(str) {
+//     return str.split('').reduce((reversed, char)=>{
+//         return char + reversed;
+//     }, '');
+// }
+
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char + rev,'');
+// }
+
+// debugger, cont, exit cntrl c
